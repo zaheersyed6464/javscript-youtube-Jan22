271fd1c5593c35f86c0adc732799274401578199
zaheersyed6464 <126182918+zaheersyed6464@users.noreply.github.com>
2024-01-23 08:30:00 +0000

    conversions types

---
+++ .devcontainer/01_basic/first.js
@@ -0,0 +1,30 @@
+let score=true
+//33
+//"33" =>3
+//"33abs" =>Nan
+//null =>0
+//undefined =>NaN
+//true /false => 1 or 0
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let typechange=Number(score);//type conversion  to integer
+console.log(typeof typechange);
+console.log(typechange);
+
+//type conversion to boolean
+ //cheaking boolean 1 / 0
+let islogged="zaheer"
+//0 =>false
+//1=> true
+//""=>false
+//"zaheer" =>true
+let booleanloggin=Boolean(islogged);
+console.log(booleanloggin);
+
+//type conversion to string
+let stringnumber=33
+let checks=String(stringnumber)
+console.log(checks);
+console.log(typeof(checks));
