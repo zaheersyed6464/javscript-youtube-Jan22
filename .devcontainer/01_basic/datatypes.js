012eb109c58575dc54fb3b0033fb9ce26bac61e0
zaheersyed6464 <126182918+zaheersyed6464@users.noreply.github.com>
2024-02-01 14:40:01 +0000

    data types summary

---
+++ .devcontainer/01_basic/datatypes.js
@@ -0,0 +1,21 @@
+//primitive
+//string,number,boolean,null,undefined,symbol,bigint
+ let z=Symbol('123')
+ let z1=Symbol('123')
+ console.log(z==z1);//false
+
+
+//non-primitive
+//arrays,objects,functions
+const hero=["pk","mb" ,"bk"]
+let obj={
+    Name:"zaheer",
+    age:21
+}
+
+
+const myf=function(){
+ console.log("z");
+}
+
+ 
