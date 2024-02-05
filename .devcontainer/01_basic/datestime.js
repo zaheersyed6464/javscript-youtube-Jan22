1eda5ae2a55df7e63d92f13786f51888990ec0f4
zaheersyed6464 <126182918+zaheersyed6464@users.noreply.github.com>
2024-02-05 15:42:52 +0000

    dates and times

---
+++ .devcontainer/01_basic/datestime.js
@@ -0,0 +1,12 @@
+let mydate=new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString()); 
+console.log(typeof mydate);
+console.log(mydate.getDay  );
+console.log(`today time is ${mydate.getDate()}`);
+
+
+const created=new Date(2024,0,12);
+console.log(created.toDateString());
