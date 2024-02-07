0a237fd480f6638d04bfab1ae2f5a05b9eabb3ae
zaheersyed6464 <126182918+zaheersyed6464@users.noreply.github.com>
2024-02-07 09:46:28 +0000

    json formates

---
+++ .devcontainer/01_basic/objectsfinal.js
@@ -0,0 +1,24 @@
+//de-structure
+
+const id={
+
+    channel:"chai aur code",
+    price:"999",
+    teach:"hitesh"
+}
+
+const {teach:z}=id
+console.log(z);
+
+//json
+// {
+//     channel:"chai aur code",
+//     price:"999",
+//     teach:"hitesh"
+// }
+
+[
+    {},
+    {},
+    {}
+]
