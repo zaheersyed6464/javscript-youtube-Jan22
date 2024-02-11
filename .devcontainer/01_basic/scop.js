5dae33f97dc3a4f031ab8bc782d372ba2ea14243
zaheersyed6464 <126182918+zaheersyed6464@users.noreply.github.com>
2024-02-11 17:11:32 +0000

    scope and functions

--- .devcontainer/01_basic/scop.js
+++ .devcontainer/01_basic/scop.js
@@ -5,6 +5,49 @@ if(true){
     const b=10
     var c=11
 }
-console.log(a);
-console.log(c);
+// console.log(a);
+// console.log(c);
+
+function one(){
+    const user= "zaheer"
+
+    function two(){
+        const user1="samii"
+        console.log(user);
+
+    }
+   // console.log(user1);
+    two()
+}
+//one()
+
+if(true){
+    const web="utube"
+    if(web==="utube"){
+        const web1=" chrromee"
+      //  console.log(web1+web);
+
+    }
+   // console.log(web1);
+   //becoz only in nested if block available
+}
+//console.log(web);
+
+
+
+// ********************interesting***********
+
+console.log(addone(5))
+//console.log(addtow(2))
+//not callable becoz function assigned to a var
+function addone(num){
+    return num+num
+}
+
+
+const  addtow=function(num){
+    return num+2
+}
+
+
 
